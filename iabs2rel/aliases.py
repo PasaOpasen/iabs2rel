f43d1bceb64e6ef71dbd6822a9d273ee5c244a56
demetry pascal <qtckpuhdsa@gmail.com>
2025-01-11 12:17:17 +0300

    -

--- iabs2rel/aliases.py
+++ iabs2rel/aliases.py
@@ -1,2 +1,9 @@
 
+from typing import Union
+from typing_extensions import TypeAlias
+
+import os
+
+
+PathLike: TypeAlias = Union[str, os.PathLike]
 
--- iabs2rel/utils.py
+++ iabs2rel/utils.py
@@ -1,3 +1,25 @@
 
+from pathlib import Path
+
+from .aliases import PathLike
+
+
+def mkdir(path: PathLike):
+    """mkdir with parents"""
+    Path(path).mkdir(parents=True, exist_ok=True)
+
+
+def mkdir_of_file(file_path: PathLike):
+    mkdir(Path(file_path).parent)
+
+
+def read_text(result_path: PathLike, encoding: str = 'utf-8'):
+    return Path(result_path).read_text(encoding=encoding)
+
+
+def write_text(result_path: PathLike, text: str, encoding: str = 'utf-8'):
+    Path(result_path).write_text(text, encoding=encoding)
+
+
 
 
